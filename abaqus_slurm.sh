78b6c238ff5a7bc830c0aa3e66f2f42293a364b1
Reuben Kraft <reuben.kraft@gmail.com>
2024-12-09 14:40:24 -0500

    Add files via upload

---
+++ abaqus_slurm.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+  
+#### specify the job and project name
+#SBATCH --job-name=abaqus
+#SBATCH --account=rhk12
+
+#### specify the required resources
+#SBATCH --account=open
+#SBATCH --partition=open
+#SBATCH --nodes=1                 # Number of nodes
+#SBATCH --ntasks-per-node=8       # Number of tasks per node (MPI processes)
+#SBATCH --mem=128GB               # Total memory allocation
+#SBATCH --time=30:00:00           # Job time limit (hh:mm:ss)
+
+# Load necessary modules
+source ~/.bashrc
+module load abaqus
+
+# Change to the directory containing the job file
+cd $SLURM_SUBMIT_DIR
+
+# Ensure Abaqus uses the correct number of CPUs
+total_cpus=$SLURM_NTASKS  # Total tasks (ntasks-per-node * nodes)
+
+# Run the Abaqus job using MPI
+abaqus job=Job-1 input=Job-1.inp cpus=$total_cpus mp_mode=mpi interactive
+
